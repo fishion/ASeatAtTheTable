1f68922ab8a8e558e058ddd35f92bc6977806c2d
Alex <alex@monney.me>
2021-02-26 00:01:08 +0000

    defer script load instead of  DOMContentLoaded

--- templates/script/site.js
+++ templates/script/site.js
@@ -1,17 +1,14 @@
+// Add events for slideout menu
 const menuEvents = require('./lib/menu-events.js');
+menuEvents.openCloseSlideoutMenu();
+
+// Add events to contact us form
 const formEvents = require('./lib/contact-form-events.js');
+const form = document.getElementsByTagName('form')[0];
+if (form){
+  // Add class to input/testarea element if the value contains any non-whitespace characters.
+  formEvents.addFieldHasValueClass(form);
 
-window.addEventListener('DOMContentLoaded', () => {
-  // Add events for slideout menu
-  menuEvents.openCloseSlideoutMenu();
-
-  // Add events to contact us form
-  const form = document.getElementsByTagName('form')[0];
-  if (form){
-    // Add class to input/testarea element if the value contains any non-whitespace characters.
-    formEvents.addFieldHasValueClass(form);
-
-    // Add form validation and submit behaviour
-    formEvents.addValidateAndSubmit(form)
-  }
-});
+  // Add form validation and submit behaviour
+  formEvents.addValidateAndSubmit(form)
+}
