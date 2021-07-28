2cf44fd5b25af6b3cd52250f3534827b1962f3db
Alex <alex@monney.me>
2021-07-28 10:58:04 +0100

    rename scripts dir to js

---
+++ docs/js/site.js
@@ -0,0 +1,21 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/site.js ***!
+  \************************/
+// Add events for slideout menu
+// load script with defer attribute to ensure DOM is loaded before execution
+
+const opener = document.getElementsByClassName('hamburger')[0];
+const closer = document.getElementsByClassName('menuX')[0];
+const popoutMenu = document.getElementsByClassName('popout')[0];
+
+opener.addEventListener('click', () => {
+  popoutMenu.classList.add('open')
+})
+
+closer.addEventListener('click', () => {
+  popoutMenu.classList.remove('open')
+})
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@
 const path = require('path');
 module.exports = {
   entry: {
-    site : './src/script/site.js',
-    formevents : './src/script/formevents.js',
+    site : './src/js/site.js',
+    formevents : './src/js/formevents.js',
   },
   mode: 'development', devtool: false,
   //mode: 'production',
   target: 'node',
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, 'docs/script'),
+    path: path.join(__dirname, 'docs/js'),
     //libraryTarget: 'commonjs'
   }
 }
